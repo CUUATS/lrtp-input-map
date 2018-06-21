98adccc3bdc92ec5b35d3f2e1a82882ede5973d7
Matt Yoder <myoder@ccrpc.org>
2018-06-21 08:38:32 -0500

    Added Spanish translations.

---
+++ src/components/i18n/es.ts
@@ -0,0 +1,55 @@
+export const phrases = {
+  "lrtp": {
+    "app": {
+      "label": "C-U Voces de Transporte",
+      "basemap": {
+        "imagery": "Imágenes",
+        "hybrid": "Imágenes con etiquetas"
+      },
+      "comment": {
+        "label": "Comentarios",
+        "location": "Elija una ubicación",
+        "add": "Añada un comentario",
+        "added": "Tu comentario ha sido agregado.",
+        "moderation": "Tu comentario está esperando ser moderado.",
+        "error": "Ocurrió un error. Por favor, inténtelo de nuevo más tarde."
+      },
+      "intro": {
+        "next": "Siguiente",
+        "prev": "Anterior",
+        "done": "Hecho",
+        "close": "Cerrado",
+        "welcome": {
+          "title": "Bienvenido",
+          "text": "Bienvenido a %{app_label} por la Comisión de Planificación Regional del Condado de Champaign. Puede navegar por el mapa para ver comentarios."
+        },
+        "view": {
+          "title": "Ver comentarios",
+          "text": "El panel de comentarios muestra los detalles de todos los comentarios actualmente visibles en el mapa."
+        },
+        "locate": {
+          "title": "Encuentra un comentario",
+          "text": "Toca el botón de localizar para acercar el mapa a un comentario."
+        },
+        "like": {
+          "title": "Te gusta el comentario",
+          "text": "Muestra tu apoyo para un comentario tocando la estrella."
+        },
+        "add": {
+          "title": "Añada un comentario",
+          "text": "Para agregar un comentario, toque el botón más, seleccione una ubicación y complete el formulario de comentarios."
+        },
+        "survey": {
+          "title": "Tome la Encuesta",
+          "text": "Tap the lightbulb to take a brief survey about the future of transportation in our region. Happy browsing!"
+        }
+      }
+    },
+    "survey-controller": {
+      "title": "Tome la Encuesta de Transporte",
+      "text": "Por favor, comparta sus ideas sobre el futuro del transporte en nuestra comunidad mediante una breve encuesta. Puede acceder a la encuesta más tarde utilizando el ícono% {icon} en la barra de herramientas localizada en la parte superior.",
+      "later": "Hazlo más tarde",
+      "now": "Hazlo ahora"
+    }
+  }
+};
